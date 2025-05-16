c521347f3cf8c8e4b91de9f5ad4aebbaa9a9fe08
rafaelbarbarroxa <rafaelbarbarroxa@gmail.com>
2025-05-16 18:28:55 +0100

    Removed controller redudancy

--- backend/src/main/java/org/acme/LabseqController.java
+++ backend/src/main/java/org/acme/LabseqController.java
@@ -4,6 +4,7 @@ import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 import org.acme.services.LabseqService;
 
 import java.math.BigInteger;
@@ -22,6 +23,7 @@ public class LabseqController {
     /**
      * Endpoint that returns the nth labseq number
      * Example usage: GET /labseq/10
+     *
      * @param n the index of the sequence value to be returned, must be a non-negative value
      * @return the value present at index n in the labseq sequence, calculated or retrieved from cache
      */
@@ -30,14 +32,14 @@ public class LabseqController {
     @Produces(MediaType.APPLICATION_JSON)
     public BigInteger labseqCalculation(@PathParam("n") BigInteger n) {
 
-        if(n.compareTo(BigInteger.ZERO)<0){
-            throw new WebApplicationException("Input must be non-negative", 400);
-        }
-
         try {
             return labseqService.labseqCalculation(n);
-        } catch (Exception e){
-            throw new WebApplicationException(e.getMessage());
+        } catch (IllegalArgumentException e) {
+            throw new WebApplicationException(
+                Response.status(Response.Status.BAD_REQUEST)
+                        .entity("Input 'n' must be a non-negative value")
+                        .build()
+            );
         }
 
     }
