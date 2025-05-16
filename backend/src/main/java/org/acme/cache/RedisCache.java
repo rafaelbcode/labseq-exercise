c1bf9d1fe336adca7aa846283509deddb3425c24
rafaelbarbarroxa <rafaelbarbarroxa@gmail.com>
2025-05-16 17:05:26 +0100

    RedisCache documentation

--- backend/src/main/java/org/acme/cache/RedisCache.java
+++ backend/src/main/java/org/acme/cache/RedisCache.java
@@ -8,20 +8,37 @@ import jakarta.inject.Inject;
 import java.math.BigInteger;
 import java.util.Optional;
 
+/**
+ * Cache class implemented using RedisDataSource, used to store labseq values to increase performance
+ */
 @ApplicationScoped
 public class RedisCache {
 
     private ValueCommands<String, BigInteger> commands;
 
+    /**
+     * Constructor method to inject the RedisDataSource
+     * @param dataSource the RedisDataSource
+     */
     @Inject
     public RedisCache(RedisDataSource dataSource) {
         this.commands = dataSource.value(BigInteger.class);
     }
 
+    /**
+     * Method to retrieve a cached BigInteger value
+     * @param key the String that represents the key
+     * @return the BigInteger value linked to the key
+     */
     public Optional<BigInteger> get(String key) {
         return Optional.ofNullable(commands.get(key));
     }
 
+    /**
+     * Method to insert a key,value pair into the cache
+     * @param key the String representing the key
+     * @param value the BigInteger value
+     */
     public void set(String key, BigInteger value){
         commands.set(key,value);
     }
