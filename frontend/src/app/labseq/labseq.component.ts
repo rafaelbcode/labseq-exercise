01eefee03c38a0c96d22318ff5a6cbf233b8e31b
rafaelbarbarroxa <rafaelbarbarroxa@gmail.com>
2025-05-17 17:53:13 +0100

    Frontend with simple CSS

--- frontend/src/app/labseq/labseq.component.ts
+++ frontend/src/app/labseq/labseq.component.ts
@@ -3,6 +3,10 @@ import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {HttpClient} from '@angular/common/http';
 import {NgIf} from '@angular/common';
 
+interface LabseqResponse {
+  result: string;
+}
+
 @Component({
   selector: 'app-labseq',
   imports: [ReactiveFormsModule, NgIf, FormsModule],
@@ -24,27 +28,17 @@ export class LabseqComponent {
 
     const baseUrl = "http://localhost:8080";
 
-    this.http.get(baseUrl+`/labseq/${queryIndex}`)
+    this.http.get<LabseqResponse>(baseUrl+`/labseq/${queryIndex}`)
       .subscribe({
         next: (data) => {
-          this.calculationResult = data;
+          this.calculationResult = data.result;
           this.isLoading = false;
         },
         error: (err) => {
-          this.calculationResult = "An error has ocurred, negative values aren't allowed."
+          this.calculationResult = "An error has occurred. Only non-negative integer numbers are allowed."
           this.isLoading = false;
         }
       });
   }
 }
 
-/**
- * next: (data) => {
- *           this.calculationResult = data;
- *           this.isLoading = false;
- *         },
- *         error: () => {
- *           this.calculationResult = 'Error occurred';
- *           this.isLoading = false;
- *         }
- */
