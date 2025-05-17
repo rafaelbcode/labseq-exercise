360dcc620799cd31c3ada0d3b116e50c1db37901
rafaelbarbarroxa <rafaelbarbarroxa@gmail.com>
2025-05-17 15:02:46 +0100

    Added SwaggerUI annotations

--- backend/src/main/java/org/acme/rest/LabseqController.java
+++ backend/src/main/java/org/acme/rest/LabseqController.java
@@ -6,6 +6,10 @@ import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import org.acme.services.LabseqService;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
 
 import java.math.BigInteger;
 
@@ -27,10 +31,18 @@ public class LabseqController {
      * @param n the index of the sequence value to be returned, must be a non-negative value
      * @return the value present at index n in the labseq sequence, calculated or retrieved from cache
      */
+    @Operation(summary = "Get a product by id", description = "Returns a product as per the id")
+    @APIResponses(value = {
+            @APIResponse(responseCode = "200", description = "The labseq sequence value was successfully retrieved."),
+            @APIResponse(responseCode = "400", description = "Bad Request - The index in the request is invalid.")
+    })
+
     @GET
     @Path("/{n}")
     @Produces(MediaType.APPLICATION_JSON)
-    public BigInteger labseqCalculation(@PathParam("n") BigInteger n) {
+    public BigInteger labseqCalculation(
+            @Parameter(name = "n", description = "Index of the labseq sequence, non-negative integer")
+            @PathParam("n") BigInteger n) {
 
         try {
             return labseqService.labseqCalculation(n);
